a3c6762ebe11c2a240258a985b84309c0778c390
Emad Alhamou <emadalhamo@gmail.com>
2021-12-20 08:56:53 +0100

    code formating

--- googleCalendarController.js
+++ googleCalendarController.js
@@ -3,42 +3,42 @@ const { google } = require('googleapis')
 const tz = require("./timeZone.json")
 
 
-const googleCalendarController = (function(){
+const googleCalendarController = (function () {
 
     const obj = {}
-    const scopes = ["https://www.googleapis.com/auth/calendar","https://www.googleapis.com/auth/calendar.events"]
+    const scopes = ["https://www.googleapis.com/auth/calendar", "https://www.googleapis.com/auth/calendar.events"]
     let timeZone = "Europe/Berlin"
 
 
-    
+
     // obj.initTimeZone = (function(){
-    
+
     //     const {timezones} = tz.find(data=> data.country_code === "DE")
 
     //     timeZone = timezones[0]
 
     // })()
 
-    obj.initOAuth2 = (function(){
+    obj.initOAuth2 = (function () {
 
         // Require oAuth2 from our google instance.
         const { OAuth2 } = google.auth
 
         // Create a new instance of oAuth and set our Client ID & Client Secret.
-        return new OAuth2(process.env.GOOGLE_CLIENT_ID,process.env.CLIENT_SECRET,process.env.REDIRECT_URL)
+        return new OAuth2(process.env.GOOGLE_CLIENT_ID, process.env.CLIENT_SECRET, process.env.REDIRECT_URL)
 
     })()
 
 
-    obj.setAppointment = async function(){
+    obj.setAppointment = async function () {
 
 
-        
-
-        return new Promise(async (resolve, reject)=>{
 
 
-            try{
+        return new Promise(async (resolve, reject) => {
+
+
+            try {
 
                 // Create a new calender instance.
                 const calendar = google.calendar({ version: 'v3', auth: obj.initOAuth2 })
@@ -46,38 +46,40 @@ const googleCalendarController = (function(){
 
                 // init the event object.
                 const eventObj = obj.createEvent()
-                
+
                 // refresh the access_token.
                 await obj.refreshAccessToken()
 
                 // Check if we a busy and have an event on our calendar for the same time.
-                const {data} = await calendar.freebusy.query({resource: {
-                                                                timeMin: eventObj.eventStartTime,
-                                                                timeMax: eventObj.eventEndTime,
-                                                                timeZone: timeZone,
-                                                                items: [{ id: 'primary' }]}
-                                                            })
-                                                                
+                const { data } = await calendar.freebusy.query({
+                    resource: {
+                        timeMin: eventObj.eventStartTime,
+                        timeMax: eventObj.eventEndTime,
+                        timeZone: timeZone,
+                        items: [{ id: 'primary' }]
+                    }
+                })
+
 
                 // Check if event array is empty which means we are not busy
-                if ((data.calendars.primary.busy).length === 0){
+                if ((data.calendars.primary.busy).length === 0) {
 
                     // If we are not busy create a new calendar event.
                     calendar.events.insert({ calendarId: 'primary', resource: eventObj }, (err, event) => {
 
                         if (err) throw err
-                        
+
                         console.log("Send Event, OK 200")
                         resolve(event)
-                        
+
                     })
-                    
+
                 } else {
-                    
+
                     throw "Sorry I'm busy..."
                 }
 
-            }catch(error){
+            } catch (error) {
                 console.log(error)
                 reject(error)
             }
@@ -86,21 +88,21 @@ const googleCalendarController = (function(){
         })
 
 
-        
+
 
     }
 
-    obj.getUrlAuth = async function(){
+    obj.getUrlAuth = async function () {
 
-        return new Promise((resolve, reject)=>{
+        return new Promise((resolve, reject) => {
 
-            try{
+            try {
 
-                const url = obj.initOAuth2.generateAuthUrl({ access_type: 'offline', scope: scopes});
+                const url = obj.initOAuth2.generateAuthUrl({ access_type: 'offline', scope: scopes });
 
                 resolve(url)
 
-            } catch(error){
+            } catch (error) {
 
                 reject(error)
             }
@@ -112,31 +114,18 @@ const googleCalendarController = (function(){
 
     }
 
-    obj.provideObjectData = async function(data){
+    obj.provideObjectData = async function (data) {
 
-        console.log(data)
+        try {
+            const { tokens } = await obj.initOAuth2.getToken(data.code)
 
-        try{
-            const {tokens} = await obj.initOAuth2.getToken(data.code)
-        
             console.log(tokens)
-
-
-            // refresh_token in the response on the first authorisation
-            // tokens["refresh_token"] = process.env.REFRESH_TOKEN
-    
-            // refresh_token in the response on the first authorisation
-            // TODO, delete access token just for testing.
-            // delete tokens["access_token"]
-            
-            // console.log(tokens)
 
             // Call the setCredentials method on our oauth2Client instance and set our refresh token.
             obj.initOAuth2.setCredentials(tokens);
-    
-            // return await googleCalendarController.setAppointment()
 
-        } catch(error){
+
+        } catch (error) {
 
             throw error
         }
@@ -144,9 +133,9 @@ const googleCalendarController = (function(){
 
     }
 
-    obj.refreshAccessToken = async function(){
+    obj.refreshAccessToken = async function () {
 
-        try{
+        try {
 
             obj.initOAuth2.credentials = {
                 refresh_token: process.env.REFRESH_TOKEN
@@ -154,13 +143,11 @@ const googleCalendarController = (function(){
 
             const access_token = await obj.initOAuth2.getAccessToken()
 
-            // console.log(access_token)
-            obj.initOAuth2.credentials = {access_token : access_token.token}
+            obj.initOAuth2.credentials = { access_token: access_token.token }
 
 
-            // return await obj.setAppointment()
 
-        } catch(error){
+        } catch (error) {
             console.log(error)
             throw error
         }
@@ -168,7 +155,7 @@ const googleCalendarController = (function(){
 
     }
 
-    obj.createEvent = function(){
+    obj.createEvent = function () {
 
         // Create a new event start date instance for temp uses in our calendar.
         const eventStartTime = new Date()
@@ -188,21 +175,21 @@ const googleCalendarController = (function(){
 
         // Create a dummy event for temp uses in our calendar
         return {
-                    summary: `Node.js Calendar App`,
-                    location: `3595 California St, San Francisco, CA 94118`,
-                    description: `Meet with David to talk about the new client project and how to integrate the calendar for booking.`,
-                    colorId: 2,
-                    start: {
-                        dateTime: eventStartTime,
-                        timeZone: timeZone,
-                    },
-                    end: {
-                        dateTime: eventEndTime,
-                        timeZone: timeZone,
-                    },
-                    eventStartTime,
-                    eventEndTime
-                }
+            summary: `Node.js Calendar App`,
+            location: `3595 California St, San Francisco, CA 94118`,
+            description: `Meet with David to talk about the new client project and how to integrate the calendar for booking.`,
+            colorId: 2,
+            start: {
+                dateTime: eventStartTime,
+                timeZone: timeZone,
+            },
+            end: {
+                dateTime: eventEndTime,
+                timeZone: timeZone,
+            },
+            eventStartTime,
+            eventEndTime
+        }
 
     }
 
